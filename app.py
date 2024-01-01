46bc3fc886516253d22e676b4f9b46362e47c004
Om Morendha <omkmorendha@gmail.com>
2024-01-01 20:19:45 +0530

    Added more upload functionality

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from llama_index import StorageContext, load_index_from_storage, VectorStoreIndex, download_loader
+from llama_index import StorageContext, load_index_from_storage, VectorStoreIndex, download_loader, SimpleDirectoryReader
 from dotenv import load_dotenv 
 import os
 import pathlib
+import requests
 
 #Load API Keys
 load_dotenv()
@@ -12,10 +13,11 @@ def main():
     st.header('Custom-Made Chatbots')
     st.write("Select the data that your chatbot should be trained with:")
 
-    data = st.selectbox('Data', ('Use Existing Files', 'Upload New File directly', 'Upload file using webhook'))
+    data = st.selectbox('Data', ('Use Existing Files', 'Upload New File directly', 'Upload file using webhook', 'Upload CSV'))
     
     #Rebuild storage context and load index
     storage_context = StorageContext.from_defaults(persist_dir='./storage')
+    
     index = load_index_from_storage(storage_context)
     
     if data == 'Use Existing Files':
@@ -27,7 +29,7 @@ def main():
         if button:
             st.write(query_engine.query(query).response)
                      
-    elif data == 'Upload New File directly':
+    elif data == 'Upload CSV':
         uploaded_file = st.file_uploader("Choose a CSV file")
         if uploaded_file is not None:
             bytes_data = uploaded_file.getvalue()
@@ -53,7 +55,7 @@ def main():
                 destination_file = open(complete_name, "w+")
                 destination_file.write(data)
                 destination_file.close()
-                st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
+                st.session_state["upload_state"] = "Saved successfully!"
 
         st.button("Upload File", on_click=upload)
         
@@ -70,10 +72,67 @@ def main():
             
             if button:
                 st.write(query_engine.query(query).response)
+            
+            save_button = st.button('Save Data Permanently')
+            if save_button:
+                index.storage_context.persist()
+                index = load_index_from_storage(storage_context)
+    
+    elif data == 'Upload New File directly':
+        uploaded_files = st.file_uploader("Upload a file", accept_multiple_files=True)
+        upload_state = st.text_area("Upload State", "", key="upload_state")
+        complete_names = []
+        
+        if uploaded_files is not None:
+            for uploaded_file in uploaded_files:
+                data = uploaded_file.getvalue()
+                parent_path = pathlib.Path(__file__).parent.resolve()           
+                save_path = os.path.join(parent_path, "data")
+                complete_name = os.path.join(save_path, uploaded_file.name)
+                complete_names.append(complete_name)    
+        
+        def upload():
+            if uploaded_files is None:
+                st.session_state["upload_state"] = "Upload a file first!"
+            else:
+                for complete_name in complete_names:
+                    destination_file = open(complete_name, "wb+")
+                    destination_file.write(data)
+                    destination_file.close()
+                    st.session_state["upload_state"] = "Saved successfully!"
 
+        st.button("Upload File", on_click=upload)
+        documents = SimpleDirectoryReader("data").load_data()
+
+        #Construct the index with the Documents
+        index = VectorStoreIndex.from_documents(documents)
+        query = st.text_input('Enter Your Query')
+        button = st.button(f'Response')
+        query_engine = index.as_query_engine()
+            
+        if button:
+            st.write(query_engine.query(query).response)
+            
+        save_button = st.button('Save Data Permanently')
+        if save_button:
+            index.storage_context.persist()
+            index = load_index_from_storage(storage_context)
         
     elif data == 'Upload file using webhook':
-        pass
-    
+        url = st.text_input('Enter URL')  
+        file_name = st.text_input('Enter File Name')  
+        
+        def url_download():
+            r = requests.get(url, stream = True, allow_redirects=True)        
+            
+            full_file_name = "./data/" + file_name
+            with open(full_file_name,"wb") as f: 
+                for chunk in r.iter_content(chunk_size=1024): 
+                
+                    if chunk: 
+                        f.write(chunk) 
+        
+        st.button("Upload File", on_click=url_download)
+        
 if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import openai
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, TreeIndex
 from dotenv import load_dotenv 
 
 #Load API Keys
 load_dotenv()
 
 #Load Custom Data
-documents = SimpleDirectoryReader('data').load_data()
+documents = SimpleDirectoryReader("data").load_data()
 
 #Construct the index with the Documents
 index = VectorStoreIndex.from_documents(documents)
