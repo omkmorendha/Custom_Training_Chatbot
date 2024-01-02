37c01e03fd0ab46a03d2039af811f4cdcfbfdca4
Om Morendha <omkmorendha@gmail.com>
2024-01-02 15:15:43 +0530

    Code Refactor

--- app.py
+++ app.py
@@ -7,104 +7,137 @@ import requests
 
 #Load API Keys
 load_dotenv()
+#Rebuild storage context and load index
+storage_context = StorageContext.from_defaults(persist_dir='./storage')
+index = load_index_from_storage(storage_context)
 
-def main():
+def save_index():
+    #Load Custom Data
+    documents = SimpleDirectoryReader("data").load_data()
 
-    st.header('Custom-Made Chatbots')
-    st.write("Select the data that your chatbot should be trained with:")
+    #Construct the index with the Documents
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist()
 
-    data = st.selectbox('Data', ('Use Existing Files', 'Upload New File directly', 'Upload file using webhook', 'Upload CSV', 'Manage Files'))
-    
-    #Rebuild storage context and load index
-    storage_context = StorageContext.from_defaults(persist_dir='./storage')
-    
+def exisiting_files():
     index = load_index_from_storage(storage_context)
-    
-    if data == 'Use Existing Files':
-        index = load_index_from_storage(storage_context)
+    query = st.text_input('Enter Your Query')
+    button = st.button(f'Response')
+    query_engine = index.as_query_engine()
+            
+    if button:
+        st.write(query_engine.query(query).response)
+
+def upload_csv():
+    uploaded_file = st.file_uploader("Choose a CSV file")
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        data = uploaded_file.getvalue().decode('utf-8').splitlines()         
+        st.session_state["preview"] = ''
+        for i in range(0, min(5, len(data))):
+            st.session_state["preview"] += data[i]
+                
+        preview = st.text_area("CSV Preview", "", height=150, key="preview")
+        
+    upload_state = st.text_area("Upload State", "", key="upload_state")
+        
+    if uploaded_file is not None:
+        data = uploaded_file.getvalue().decode('utf-8')
+        parent_path = pathlib.Path(__file__).parent.resolve()           
+        save_path = os.path.join(parent_path, "data")
+        complete_name = os.path.join(save_path, uploaded_file.name)       
+        
+    def upload():
+        if uploaded_file is None:
+            st.session_state["upload_state"] = "Upload a file first!"
+        else:
+            destination_file = open(complete_name, "w+")
+            destination_file.write(data)
+            destination_file.close()
+            st.session_state["upload_state"] = "Saved successfully!"
+
+    st.button("Upload File", on_click=upload)
+        
+    if st.session_state["upload_state"][:4] == "Save":
+        SimpleCSVReader = download_loader("SimpleCSVReader")
+        loader = SimpleCSVReader(encoding="utf-8")
+        documents = loader.load_data(file=pathlib.Path(complete_name))
+        
+        index = VectorStoreIndex.from_documents(documents)
+            
         query = st.text_input('Enter Your Query')
         button = st.button(f'Response')
         query_engine = index.as_query_engine()
             
         if button:
             st.write(query_engine.query(query).response)
-                     
-    elif data == 'Upload CSV':
-        uploaded_file = st.file_uploader("Choose a CSV file")
-        if uploaded_file is not None:
-            bytes_data = uploaded_file.getvalue()
-            data = uploaded_file.getvalue().decode('utf-8').splitlines()         
-            st.session_state["preview"] = ''
-            for i in range(0, min(5, len(data))):
-                st.session_state["preview"] += data[i]
-                
-            preview = st.text_area("CSV Preview", "", height=150, key="preview")
+            
+        save_button = st.button('Save Data Permanently')
+        if save_button:
+            index.storage_context.persist()
+            index = load_index_from_storage(storage_context)    
+
+def upload_direct():
+    uploaded_files = st.file_uploader("Upload a file", accept_multiple_files=True)
+    upload_state = st.text_area("Upload State", "", key="upload_state")
+    complete_names = []
         
-        upload_state = st.text_area("Upload State", "", key="upload_state")
-        
-        if uploaded_file is not None:
-            data = uploaded_file.getvalue().decode('utf-8')
+    if uploaded_files is not None:
+        for uploaded_file in uploaded_files:
+            data = uploaded_file.getvalue()
             parent_path = pathlib.Path(__file__).parent.resolve()           
             save_path = os.path.join(parent_path, "data")
-            complete_name = os.path.join(save_path, uploaded_file.name)       
+            complete_name = os.path.join(save_path, uploaded_file.name)
+            complete_names.append(complete_name)    
         
-        def upload():
-            if uploaded_file is None:
-                st.session_state["upload_state"] = "Upload a file first!"
-            else:
-                destination_file = open(complete_name, "w+")
+    def upload():
+        if uploaded_files is None:
+            st.session_state["upload_state"] = "Upload a file first!"
+        else:
+            for complete_name in complete_names:
+                destination_file = open(complete_name, "wb+")
                 destination_file.write(data)
                 destination_file.close()
                 st.session_state["upload_state"] = "Saved successfully!"
 
-        st.button("Upload File", on_click=upload)
-        
-        if st.session_state["upload_state"][:4] == "Save":
-            SimpleCSVReader = download_loader("SimpleCSVReader")
-            loader = SimpleCSVReader(encoding="utf-8")
-            documents = loader.load_data(file=pathlib.Path(complete_name))
-        
-            index = VectorStoreIndex.from_documents(documents)
-            
-            query = st.text_input('Enter Your Query')
-            button = st.button(f'Response')
-            query_engine = index.as_query_engine()
-            
-            if button:
-                st.write(query_engine.query(query).response)
-            
-            save_button = st.button('Save Data Permanently')
-            if save_button:
-                index.storage_context.persist()
-                index = load_index_from_storage(storage_context)
-    
-    elif data == 'Upload New File directly':
-        uploaded_files = st.file_uploader("Upload a file", accept_multiple_files=True)
-        upload_state = st.text_area("Upload State", "", key="upload_state")
-        complete_names = []
-        
-        if uploaded_files is not None:
-            for uploaded_file in uploaded_files:
-                data = uploaded_file.getvalue()
-                parent_path = pathlib.Path(__file__).parent.resolve()           
-                save_path = os.path.join(parent_path, "data")
-                complete_name = os.path.join(save_path, uploaded_file.name)
-                complete_names.append(complete_name)    
-        
-        def upload():
-            if uploaded_files is None:
-                st.session_state["upload_state"] = "Upload a file first!"
-            else:
-                for complete_name in complete_names:
-                    destination_file = open(complete_name, "wb+")
-                    destination_file.write(data)
-                    destination_file.close()
-                    st.session_state["upload_state"] = "Saved successfully!"
+    st.button("Upload File", on_click=upload)
+    documents = SimpleDirectoryReader("data").load_data()
 
-        st.button("Upload File", on_click=upload)
+    #Construct the index with the Documents
+    index = VectorStoreIndex.from_documents(documents)
+    query = st.text_input('Enter Your Query')
+    button = st.button(f'Response')
+    query_engine = index.as_query_engine()
+            
+    if button:
+        st.write(query_engine.query(query).response)
+            
+    save_button = st.button('Save Data Permanently')
+    if save_button:                                                                  
+        save_index()
+        index = load_index_from_storage(storage_context)
+
+def upload_webhook():
+    url = st.text_input('Enter URL')  
+    file_name = st.text_input('Enter File Name')  
+    upload_state = False
+    
+    def url_download():
+        r = requests.get(url, stream = True, allow_redirects=True)        
+        
+        full_file_name = "./data/" + file_name
+        with open(full_file_name,"wb") as f:
+            upload_state = True 
+            for chunk in r.iter_content(chunk_size=1024): 
+            
+                if chunk: 
+                    f.write(chunk) 
+    
+    st.button("Upload File", on_click=url_download)
+    if True:
         documents = SimpleDirectoryReader("data").load_data()
 
-        #Construct the index with the Documents
+         #Construct the index with the Documents
         index = VectorStoreIndex.from_documents(documents)
         query = st.text_input('Enter Your Query')
         button = st.button(f'Response')
@@ -114,54 +147,43 @@ def main():
             st.write(query_engine.query(query).response)
             
         save_button = st.button('Save Data Permanently')
-        if save_button:                                                                  
-            index.storage_context.persist()
-            index = load_index_from_storage(storage_context)
+        if save_button:
+            save_index()
+            index = load_index_from_storage(storage_context)    
+
+def manage_files():
+    filelist=[]
+    for root, dirs, files in os.walk("data"):
+        for file in files:
+            filename=os.path.join(root, file)
+            filelist.append(file)
+                    
+            if(st.button('Delete ' + file)):
+                os.remove('./data/' + file)
+                save_index()
+                st.rerun()
+
+def main():
+
+    st.header('Custom-Made Chatbots')
+    st.write("Select the data that your chatbot should be trained with:")
+
+    data = st.selectbox('Data', ('Use Existing Files', 'Upload New File directly', 'Upload file using webhook', 'Upload CSV', 'Manage Files'))
+    
+    if data == 'Use Existing Files':
+        exisiting_files()
+                     
+    elif data == 'Upload CSV':
+        upload_csv()
+    
+    elif data == 'Upload New File directly':
+        upload_direct()
         
     elif data == 'Upload file using webhook':
-        url = st.text_input('Enter URL')  
-        file_name = st.text_input('Enter File Name')  
-        upload_state = False
+        upload_webhook()
         
-        def url_download():
-            r = requests.get(url, stream = True, allow_redirects=True)        
-            
-            full_file_name = "./data/" + file_name
-            with open(full_file_name,"wb") as f:
-                upload_state = True 
-                for chunk in r.iter_content(chunk_size=1024): 
-                
-                    if chunk: 
-                        f.write(chunk) 
-        
-        st.button("Upload File", on_click=url_download)
-        if True:
-            documents = SimpleDirectoryReader("data").load_data()
-
-            #Construct the index with the Documents
-            index = VectorStoreIndex.from_documents(documents)
-            query = st.text_input('Enter Your Query')
-            button = st.button(f'Response')
-            query_engine = index.as_query_engine()
-                
-            if button:
-                st.write(query_engine.query(query).response)
-                
-            save_button = st.button('Save Data Permanently')
-            if save_button:
-                index.storage_context.persist()
-                index = load_index_from_storage(storage_context)
-    
     elif data == 'Manage Files':
-        filelist=[]
-        for root, dirs, files in os.walk("data"):
-            for file in files:
-                    filename=os.path.join(root, file)
-                    filelist.append(file)
-                    
-                    if(st.button('Delete ' + file)):
-                        os.remove('./data/' + file)
-                        st.rerun()
+        manage_files()
     
 if __name__ == '__main__':
     main()
