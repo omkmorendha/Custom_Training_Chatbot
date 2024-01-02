368ad728c3901eaf888082602da219527424bde4
Om Morendha <omkmorendha@gmail.com>
2024-01-02 20:11:11 +0530

    Format Code and delete old file

--- app.py
+++ app.py
@@ -1,53 +1,62 @@
 from distutils.command import upload
 import streamlit as st
-from llama_index import StorageContext, load_index_from_storage, VectorStoreIndex, download_loader, SimpleDirectoryReader
-from dotenv import load_dotenv 
+from llama_index import (
+    StorageContext,
+    load_index_from_storage,
+    VectorStoreIndex,
+    download_loader,
+    SimpleDirectoryReader,
+)
+from dotenv import load_dotenv
 import os
 import pathlib
 import requests
 
-#Load API Keys
+# Load API Keys
 load_dotenv()
-#Rebuild storage context and load index
-storage_context = StorageContext.from_defaults(persist_dir='./storage')
+# Rebuild storage context and load index
+storage_context = StorageContext.from_defaults(persist_dir="./storage")
 index = load_index_from_storage(storage_context)
 
+
 def save_index():
-    #Load Custom Data
+    # Load Custom Data
     documents = SimpleDirectoryReader("data").load_data()
 
-    #Construct the index with the Documents
+    # Construct the index with the Documents
     index = VectorStoreIndex.from_documents(documents)
     index.storage_context.persist()
 
+
 def exisiting_files():
     index = load_index_from_storage(storage_context)
-    query = st.text_input('Enter Your Query')
-    button = st.button(f'Response')
+    query = st.text_input("Enter Your Query")
+    button = st.button(f"Response")
     query_engine = index.as_query_engine()
-            
+
     if button:
         st.write(query_engine.query(query).response)
+
 
 def upload_csv():
     uploaded_file = st.file_uploader("Choose a CSV file")
     if uploaded_file is not None:
         bytes_data = uploaded_file.getvalue()
-        data = uploaded_file.getvalue().decode('utf-8').splitlines()         
-        st.session_state["preview"] = ''
+        data = uploaded_file.getvalue().decode("utf-8").splitlines()
+        st.session_state["preview"] = ""
         for i in range(0, min(5, len(data))):
             st.session_state["preview"] += data[i]
-                
+
         preview = st.text_area("CSV Preview", "", height=150, key="preview")
-        
+
     upload_state = st.text_area("Upload State", "", key="upload_state")
-        
+
     if uploaded_file is not None:
-        data = uploaded_file.getvalue().decode('utf-8')
-        parent_path = pathlib.Path(__file__).parent.resolve()           
+        data = uploaded_file.getvalue().decode("utf-8")
+        parent_path = pathlib.Path(__file__).parent.resolve()
         save_path = os.path.join(parent_path, "data")
-        complete_name = os.path.join(save_path, uploaded_file.name)       
-        
+        complete_name = os.path.join(save_path, uploaded_file.name)
+
     def upload():
         if uploaded_file is None:
             st.session_state["upload_state"] = "Upload a file first!"
@@ -58,39 +67,40 @@ def upload_csv():
             st.session_state["upload_state"] = "Saved successfully!"
 
     st.button("Upload File", on_click=upload)
-        
+
     if st.session_state["upload_state"][:4] == "Save":
         SimpleCSVReader = download_loader("SimpleCSVReader")
         loader = SimpleCSVReader(encoding="utf-8")
         documents = loader.load_data(file=pathlib.Path(complete_name))
-        
+
         index = VectorStoreIndex.from_documents(documents)
-            
-        query = st.text_input('Enter Your Query')
-        button = st.button(f'Response')
+
+        query = st.text_input("Enter Your Query")
+        button = st.button(f"Response")
         query_engine = index.as_query_engine()
-            
+
         if button:
             st.write(query_engine.query(query).response)
-            
-        save_button = st.button('Save Data Permanently')
+
+        save_button = st.button("Save Data Permanently")
         if save_button:
             index.storage_context.persist()
-            index = load_index_from_storage(storage_context)    
+            index = load_index_from_storage(storage_context)
+
 
 def upload_direct():
     uploaded_files = st.file_uploader("Upload a file", accept_multiple_files=True)
     upload_state = st.text_area("Upload State", "", key="upload_state")
     complete_names = []
-        
+
     if uploaded_files is not None:
         for uploaded_file in uploaded_files:
             data = uploaded_file.getvalue()
-            parent_path = pathlib.Path(__file__).parent.resolve()           
+            parent_path = pathlib.Path(__file__).parent.resolve()
             save_path = os.path.join(parent_path, "data")
             complete_name = os.path.join(save_path, uploaded_file.name)
-            complete_names.append(complete_name)    
-        
+            complete_names.append(complete_name)
+
     def upload():
         if uploaded_files is None:
             st.session_state["upload_state"] = "Upload a file first!"
@@ -106,19 +116,18 @@ def upload_direct():
         save_index()
         index = load_index_from_storage(storage_context)
 
-        query = st.text_input('Enter Your Query')
-        button = st.button(f'Response')
+        query = st.text_input("Enter Your Query")
+        button = st.button(f"Response")
         query_engine = index.as_query_engine()
-                
+
         if button:
-            st.write(query_engine.query(query).response)                                                        
+            st.write(query_engine.query(query).response)
 
 
 def upload_webhook():
-    url = st.text_input('Enter URL')
-    file_name = st.text_input('Enter File Name')  
+    url = st.text_input("Enter URL")
+    file_name = st.text_input("Enter File Name")
 
-    
     def url_download(file_name):
         try:
             r = requests.get(url, stream=True, allow_redirects=True)
@@ -135,60 +144,69 @@ def upload_webhook():
             st.session_state["upload_state"] = "Saved " + file_name + " successfully!"
         except requests.exceptions.RequestException as e:
             st.session_state["upload_state"] = "Failed to download: " + str(e)
-    
+
     if st.button("Upload File"):
         url_download(file_name)
 
-    upload_state = st.text_area("Upload State", "", key="upload_state")  
+    upload_state = st.text_area("Upload State", "", key="upload_state")
 
     save_index()
     index = load_index_from_storage(storage_context)
 
-    query = st.text_input('Enter Your Query')
-    button = st.button(f'Response')
+    query = st.text_input("Enter Your Query")
+    button = st.button(f"Response")
     query_engine = index.as_query_engine()
-                
+
     if button:
-        st.write(query_engine.query(query).response)    
+        st.write(query_engine.query(query).response)
 
 
 def manage_files():
-    filelist=[]
+    filelist = []
     for root, dirs, files in os.walk("data"):
         for file in files:
-            if(file == '.txt'):
+            if file == ".txt":
                 continue
-            
-            filename=os.path.join(root, file)
+
+            filename = os.path.join(root, file)
             filelist.append(file)
-                    
-            if(st.button('Delete ' + file)):
-                os.remove('./data/' + file)
+
+            if st.button("Delete " + file):
+                os.remove("./data/" + file)
                 save_index()
                 st.rerun()
 
-def main():
 
-    st.header('Custom-Training Chatbot')
+def main():
+    st.header("Custom-Training Chatbot")
     st.write("Upload, Delete or Query the data using Chatbot:")
 
-    #data = st.selectbox('Option', ('Use Existing Files', 'Upload New File directly', 'Upload file using webhook', 'Upload CSV', 'Manage Files'))
-    data = st.selectbox('Option', ('Use Existing Files', 'Upload New File directly', 'Upload file using webhook', 'Manage Files'))
-    
-    if data == 'Use Existing Files':
+    # data = st.selectbox('Option', ('Use Existing Files', 'Upload New File directly', 'Upload file using webhook', 'Upload CSV', 'Manage Files'))
+    data = st.selectbox(
+        "Option",
+        (
+            "Use Existing Files",
+            "Upload New File directly",
+            "Upload file using webhook",
+            "Manage Files",
+        ),
+    )
+
+    if data == "Use Existing Files":
         exisiting_files()
-                     
-    elif data == 'Upload CSV':
+
+    elif data == "Upload CSV":
         upload_csv()
-    
-    elif data == 'Upload New File directly':
+
+    elif data == "Upload New File directly":
         upload_direct()
-        
-    elif data == 'Upload file using webhook':
+
+    elif data == "Upload file using webhook":
         upload_webhook()
-        
-    elif data == 'Manage Files':
+
+    elif data == "Manage Files":
         manage_files()
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
